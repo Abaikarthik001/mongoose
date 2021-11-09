c16a78fc56345a5114e1dcab0cd2d3cee42a9d93
Abaikarthik001 <80613260+Abaikarthik001@users.noreply.github.com>
2021-11-09 12:20:52 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const ConnectDB = async ()=>{
-    await mongoose.connect("mongodb+srv://abai001:<abai001>@cluster0.idpfx.mongodb.net/myFirstDatabase?retryWrites=true&w=majorit");
+    await mongoose.connect("mongodb+srv://abai001:abai001@cluster0.idpfx.mongodb.net/myFirstDatabase?retryWrites=true&w=majorit");
 }
 
 module.exports = ConnectDB;
